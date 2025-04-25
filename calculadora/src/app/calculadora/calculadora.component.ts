ae7856064448e1b4c84e56da15c1c69f58727243
carolgal <galeskar@gmail.com>
2025-04-24 22:45:06 -0300

    calculadora

---
+++ calculadora/src/app/calculadora/calculadora.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculadora',
+  imports: [],
+  templateUrl: './calculadora.component.html',
+  styleUrl: './calculadora.component.css'
+})
+export class CalculadoraComponent {
+
+  public numero1: number = 0;
+  public numero2: number = 0;
+  public operacao: string = 'soma';
+  public resultado: number = 0;
+
+  calcular() {
+    switch(this.operacao) {
+      case 'soma':
+        this.resultado = this.numero1 + this.numero2;
+        break;
+      case 'subtracao':
+        this.resultado = this.numero1 - this.numero2;
+        break;
+      case 'multiplicacao':
+        this.resultado = this.numero1 * this.numero2;
+        break;
+      case 'divisao':
+          if (this.numero2 !== 0) {
+            this.resultado = this.numero1 / this.numero2;
+          } else {
+            alert('não é possíbel dividir por zero');
+          }
+
+          break;
+
+    }
+  }
+
+}
+
